022ca3c2675a2a351eee317a5ec8f2b76cbc9047
田世洋 <tianshiyang@icourt.cc>
2025-05-11 23:01:18 +0800

    feat: 对话基础

--- study/8-ChatMessageHistory组件上手与源码解析/1.对话消息历史组件基础.py
+++ study/8-ChatMessageHistory组件上手与源码解析/1.对话消息历史组件基础.py
@@ -5,3 +5,11 @@
 @Author  : tianshiyang
 @File    : 1.对话消息历史组件基础.py
 """
+from langchain_core.chat_history import InMemoryChatMessageHistory
+
+memory = InMemoryChatMessageHistory()
+
+memory.add_user_message("我是慕小课，你是谁")
+memory.add_ai_message("你好，我是ChatGPT，有什么可以帮到您的？")
+
+print(memory.messages)
