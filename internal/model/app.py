29981c46f64dd2e4f4291da30c533febd4566930
田世洋 <tianshiyang@icourt.cc>
2025-05-19 19:14:14 +0800

    feat: 更新app模型

--- internal/model/app.py
+++ internal/model/app.py
@@ -8,24 +8,28 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import Column, UUID, String, PrimaryKeyConstraint, Index, DateTime
+from sqlalchemy import Column, UUID, String, PrimaryKeyConstraint, Index, DateTime, text, Text
 
 from internal.extension.database_extension import db
 
 
 class App(db.Model):
-    """AI应用基础模型类"""
     __tablename__ = 'app'
     __table_args__ = (
-        PrimaryKeyConstraint('id', name='app_id_pk'),
-        Index("idx_app_account_id", "account_id"),
+        PrimaryKeyConstraint("id", name="pk_app_id"),
+        Index("idx_app_account_id", "account_id")
     )
 
-    id = Column(UUID, default=uuid.uuid4, nullable=False)
-    account_id = Column(UUID, nullable=False)
-    name = Column(String(255), default="", nullable=False)
-    icon = Column(String(255), default="", nullable=False)
-    description = Column(String(255), default="", nullable=False)
-    status = Column(String(255), default="", nullable=False)
-    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
-    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    id = Column(UUID, nullable=False, server_default=text("uuid_generate_v4()"))
+    account_id = Column(UUID)
+    name = Column(String(255), nullable=False, server_default=text("''::character varying"))
+    icon = Column(String(255), nullable=False, server_default=text("''::character varying"))
+    description = Column(Text, nullable=False, server_default=text("''::text"))
+    status = Column(String(255), nullable=False, server_default=text("''::character varying"))
+    update_at = Column(
+        DateTime,
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP(0)"),
+        server_onupdate=text("CURRENT_TIMESTAMP(0)"),
+    )
+    create_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP(0)"))
