5016fdfc1961c65079436bf3162ddbea3dfcea18
田世洋 <tianshiyang@icourt.cc>
2025-05-19 23:50:50 +0800

    feat: 多LLM链选择

--- study/17-Runnable组件动态替换运行组件/1.多LLM链选择示例.py
+++ study/17-Runnable组件动态替换运行组件/1.多LLM链选择示例.py
@@ -5,3 +5,30 @@
 @Author  : tianshiyang
 @File    : 1.多LLM链选择示例.py
 """
+import dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import ConfigurableField
+from langchain_openai import ChatOpenAI
+
+dotenv.load_dotenv()
+
+prompt = PromptTemplate.from_template("{query}")
+llm = ChatOpenAI(model="moonshot-v1-8k").configurable_alternatives(
+    ConfigurableField(id="llm"),
+    default_key="moonshot-v1-8k",
+    gpt4=ChatOpenAI(model="gpt-4o"),
+)
+
+# 2.构建链应用
+chain = prompt | llm | StrOutputParser()
+
+# 3.调用链并传递配置信息，并切换到文心一言模型或者gpt4模型
+content = chain.with_config(
+    configurable={
+        "llm": "moonshot-v1-8k"
+    }
+).invoke(
+    {"query": "你好，你是什么模型呢?"}
+)
+print(content)
