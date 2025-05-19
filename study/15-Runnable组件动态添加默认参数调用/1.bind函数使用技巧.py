07700812fbe93e16163b014c9eba17ba2cb27ad8
田世洋 <tianshiyang@icourt.cc>
2025-05-19 22:41:32 +0800

    feat: 创建文件

--- study/15-Runnable组件动态添加默认参数调用/1.bind函数使用技巧.py
+++ study/15-Runnable组件动态添加默认参数调用/1.bind函数使用技巧.py
@@ -5,3 +5,23 @@
 @Author  : tianshiyang
 @File    : 1.bind函数使用技巧.py
 """
+import dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+dotenv.load_dotenv()
+
+prompt = ChatPromptTemplate.from_messages([
+    ("human", "{query}")
+])
+
+llm = ChatOpenAI()
+
+chain = prompt | llm.bind(model="moonshot-v1-8k") | StrOutputParser()
+
+content = chain.invoke({
+    "query": "你是什么模型呢,具体哪个版本呢"
+})
+
+print(content)
--- study/15-Runnable组件动态添加默认参数调用/2.bind解决RunnableLambda函数多参场景.py
+++ study/15-Runnable组件动态添加默认参数调用/2.bind解决RunnableLambda函数多参场景.py
@@ -5,3 +5,23 @@
 @Author  : tianshiyang
 @File    : 2.bind解决RunnableLambda函数多参场景.py
 """
+from random import randint
+
+from langchain_core.runnables import RunnableLambda
+
+
+def get_weather(location: str, unit: str, name: str):
+    """根据传入的位置+温度单位获取对应的天气信息"""
+    print(f"位置{location}")
+    print(f"unit {unit}")
+    print(f"name {name}")
+    return f"{location}天气为{randint(24, 40)}{unit}"
+
+
+get_weather_runnable = RunnableLambda(get_weather).bind(unit="摄氏度", name='慕小课')
+
+content = get_weather_runnable.invoke({
+    "location": "121231233",
+})
+
+print(content)
