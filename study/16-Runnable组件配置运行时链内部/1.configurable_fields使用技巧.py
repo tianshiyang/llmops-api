3d3f10a36f547493c987bad90833bf60fc1d3239
田世洋 <tianshiyang@icourt.cc>
2025-05-19 22:46:38 +0800

    feat: bind函数

--- study/16-Runnable组件配置运行时链内部/1.configurable_fields使用技巧.py
+++ study/16-Runnable组件配置运行时链内部/1.configurable_fields使用技巧.py
@@ -5,3 +5,40 @@
 @Author  : tianshiyang
 @File    : 1.configurable_fields使用技巧.py
 """
+import dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import ConfigurableField
+from langchain_openai import ChatOpenAI
+
+dotenv.load_dotenv()
+
+prompt = PromptTemplate.from_template("请生成一个小于{x}的整数")
+
+llm = ChatOpenAI(model="moonshot-v1-8k").configurable_fields(
+    temperature=ConfigurableField(
+        id="llm_temperature",
+        name="大语言模型的温度",
+        description="温度越低，大语言模型生成的内容越确定，温度越高，生成内容越随机"
+    )
+)
+
+chain = prompt | llm | StrOutputParser()
+
+content = chain.invoke({
+    "x": 1000
+})
+
+print(content)
+
+print("========")
+
+with_config_chain = chain.with_config(configurable={
+    "llm_temperature": 0
+})
+
+content2 = with_config_chain.invoke({
+    "x": 1000
+})
+
+print(content2)
--- study/16-Runnable组件配置运行时链内部/2.configurable_fields替换提示词.py
+++ study/16-Runnable组件配置运行时链内部/2.configurable_fields替换提示词.py
@@ -5,3 +5,36 @@
 @Author  : tianshiyang
 @File    : 2.configurable_fields替换提示词.py
 """
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import ConfigurableField
+
+prompt = PromptTemplate.from_template("请写一篇关于{subject}主题的冷笑话").configurable_fields(
+    template=ConfigurableField(id="prompt_template")
+)
+
+content = prompt.invoke({
+    "subject": "程序员",
+}).to_string()
+
+print(content)
+
+# 方法一，在invoke的config中配置参数
+# content2 = prompt.invoke({
+#     "subject": "程序员"
+# }, config={
+#     "configurable": {
+#         "prompt_template": "请写一篇关于{subject}主题的藏头诗"
+#     }
+# }).to_string()
+
+# 方法2，使用with_config
+with_config_prompt = prompt.with_config(configurable={
+    "prompt_template": "请写一篇关于{subject}主题的藏头诗2"
+})
+
+content2 = with_config_prompt.invoke({
+    "subject": "程序员",
+}).to_string()
+
+print(content2)
