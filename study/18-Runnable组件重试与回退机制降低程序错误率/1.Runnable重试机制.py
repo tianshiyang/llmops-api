d1f15bd6bb8572a262fa8753382a39c1af205120
田世洋 <tianshiyang@icourt.cc>
2025-05-19 23:55:48 +0800

    feat: 重试与回退机制

--- study/18-Runnable组件重试与回退机制降低程序错误率/1.Runnable重试机制.py
+++ study/18-Runnable组件重试与回退机制降低程序错误率/1.Runnable重试机制.py
@@ -5,3 +5,19 @@
 @Author  : tianshiyang
 @File    : 1.Runnable重试机制.py
 """
+from langchain_core.runnables import RunnableLambda
+
+count = -1
+
+
+def func(num):
+    global count
+    count += 1
+    print(f'当前值{count}')
+    return num / count
+
+
+chain = RunnableLambda(func).with_retry(stop_after_attempt=2)
+
+content = chain.invoke(2)
+print(content)
