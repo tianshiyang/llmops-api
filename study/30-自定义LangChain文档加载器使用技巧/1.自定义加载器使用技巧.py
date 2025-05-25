ed926e7619576d5c949f6c2b656faa4a74b08463
田世洋 <tianshiyang@icourt.cc>
2025-05-25 21:28:26 +0800

    feat: 自定义加载器使用

--- study/30-自定义LangChain文档加载器使用技巧/1.自定义加载器使用技巧.py
+++ study/30-自定义LangChain文档加载器使用技巧/1.自定义加载器使用技巧.py
@@ -5,3 +5,24 @@
 @Author  : tianshiyang
 @File    : 1.自定义加载器使用技巧.py
 """
+from langchain_core.document_loaders import BaseLoader
+from langchain_core.documents import Document
+
+
+class CustomDocumentLoader(BaseLoader):
+    def __init__(self, path):
+        self.path = path
+
+    def lazy_load(self):
+        with open(self.path, encoding="utf-8") as f:
+            line_number = 0
+            for line in f:
+                yield Document(page_content=line, line_number=line_number)
+                line_number += 1
+
+
+loader = CustomDocumentLoader("./喵喵.txt")
+documents = loader.load()
+print(documents)
+print(len(documents))
+print(documents[0].metadata)
