087880c451d4e18f4b6ca00d532e46da1c5fb8de
田世洋 <tianshiyang@icourt.cc>
2025-05-25 23:37:53 +0800

    feat: 字符分割器示例

--- study/32-文档转换器与文本分割器组件的使用/1.字符分割器使用示例.py
+++ study/32-文档转换器与文本分割器组件的使用/1.字符分割器使用示例.py
@@ -5,3 +5,22 @@
 @Author  : tianshiyang
 @File    : 1.字符分割器使用示例.py
 """
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_unstructured import UnstructuredLoader
+
+loader = UnstructuredLoader("./项目API文档.md")
+documents = loader.load()
+
+text_splitter = CharacterTextSplitter(
+    separator="\n\n",
+    chunk_size=500,
+    chunk_overlap=50,
+    add_start_index=True
+)
+
+chunks = text_splitter.split_documents(documents)
+
+for chunk in chunks:
+    print(f"块大小{len(chunk.page_content)}, 元数据{chunk.metadata}")
+
+print(len(chunks))
