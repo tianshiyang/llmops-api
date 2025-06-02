a3aaee003b12c89ba3fba0233c8146d0def9a5b0
田世洋 <tianshiyang@icourt.cc>
2025-06-02 15:53:05 +0800

    feat: 基于逻辑和语义分发路由

--- study/46-检索器的逻辑路由缩减检索范围/1.函数回调规范化输出.py
+++ study/46-检索器的逻辑路由缩减检索范围/1.函数回调规范化输出.py
@@ -5,3 +5,36 @@
 @Author  : tianshiyang
 @File    : 1.函数回调规范化输出.py
 """
+import os
+from typing import Literal
+
+import dotenv
+from langchain_openai import ChatOpenAI
+
+from pydantic import BaseModel, Field
+
+# from pydantic import BaseModel, Field
+
+dotenv.load_dotenv()
+
+
+class RouteQuery(BaseModel):
+    datasource: Literal["python_docs", "javascript_docs", "golang_docs"] = Field(
+        description="根据用户提问，选择哪个数据源最相关以回答用户的提问"
+    )
+
+
+# 1. 创建绑定结构化输出的大语言模型
+llm = ChatOpenAI(model=os.getenv("BASE_CHAT_MODEL"), temperature=0)
+
+structured_llm = llm.with_structured_output(RouteQuery)
+question = """为什么下面的代码不工作了，请帮我检查下：
+
+var a = "123"
+"""
+
+res = structured_llm.invoke(question)
+
+print(res)
+print(type(res))
+print(res.datasource)
--- study/46-检索器的逻辑路由缩减检索范围/2.基于逻辑和语义的路由分发.py
+++ study/46-检索器的逻辑路由缩减检索范围/2.基于逻辑和语义的路由分发.py
@@ -5,3 +5,50 @@
 @Author  : tianshiyang
 @File    : 2.基于逻辑和语义的路由分发.py
 """
+import os
+
+import dotenv
+from typing import Literal
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+
+dotenv.load_dotenv()
+
+
+class RouteQuery(BaseModel):
+    datasource: Literal["python_docs", "javascript_docs", "golang_docs"] = Field(
+        description="请根据用户的输入，判断使用的那个文档"
+    )
+
+
+def choose_route(route: RouteQuery):
+    if route.datasource == "python_docs":
+        return 'python'
+    elif route.datasource == "javascript_docs":
+        return 'javascript'
+    else:
+        return 'go'
+
+
+structured_llm = ChatOpenAI(model=os.getenv("BASE_CHAT_MODEL")).with_structured_output(RouteQuery)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "你是一个擅长将用户问题路由到适当的数据源的专家。\n请根据问题涉及的编程语言，将其路由到相关数据源"),
+    ("human", "{question}")
+])
+
+chain = {
+            "question": RunnablePassthrough()
+        } | prompt | structured_llm | choose_route
+
+question = """为什么下面的代码不工作了，请帮我检查下：
+
+from langchain_core.prompts import ChatPromptTemplate
+
+prompt = ChatPromptTemplate.from_messages(["human", "speak in {language}"])
+prompt.invoke("中文")"""
+
+print(chain.invoke(question))
