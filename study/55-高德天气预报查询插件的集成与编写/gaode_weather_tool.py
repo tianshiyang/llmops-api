2d7b5f62ebea037df72e354630b862c552ac80c6
田世洋 <tianshiyang@icourt.cc>
2025-06-06 18:12:36 +0800

    feat: 谷歌实时搜索天气

--- study/55-高德天气预报查询插件的集成与编写/gaode_weather_tool.py
+++ study/55-高德天气预报查询插件的集成与编写/gaode_weather_tool.py
@@ -5,3 +5,61 @@
 @Author  : tianshiyang
 @File    : gaode_weather_tool.py
 """
+import json
+import os
+from typing import Any, Type
+
+import dotenv
+import requests
+from langchain_core.tools import BaseTool
+from pydantic import Field, BaseModel
+
+dotenv.load_dotenv()
+request_base_url: str = "https://restapi.amap.com/v3"
+
+
+class GaoDeWeatherArgsSchema(BaseModel):
+    city: str = Field(description="需要查询的城市信息，如北京")
+
+
+class GaoDeWeatherTool(BaseTool):
+    name: str = "gaode_weather_tool"
+    description: str = "通过城市获取天气信息"
+    args_schema: Type[BaseModel] = GaoDeWeatherArgsSchema
+
+    def _run(self, *args: Any, **kwargs: Any) -> Any:
+        try:
+            city = kwargs.get("city")
+            api_key = os.getenv("GAODE_API_KEY")
+            if not api_key:
+                return f"没有配置高德api信息"
+            session = requests.session()
+            city_response = session.request(
+                method="GET",
+                url=f"{request_base_url}/config/district",
+                params={"keywords": city, "key": api_key, "subdistrict": 0},
+            )
+
+            city_data = city_response.json()
+
+            if city_data.get("info") == "OK":
+                adcode = city_data.get("districts")[0].get("adcode")
+
+                weather_response = session.request(
+                    method="GET",
+                    url=f"{request_base_url}/weather/weatherInfo",
+                    params={"city": adcode, "key": api_key, },
+                )
+                weather_data = weather_response.json()
+                if weather_data.get("info") == "OK":
+                    return json.dumps(weather_data)
+                else:
+                    return "获取高德天气失败"
+            else:
+                return f"获取省市区编码失败"
+        except Exception as e:
+            return f"获取天气失败"
+
+
+gaode_weather = GaoDeWeatherTool()
+print(gaode_weather.invoke({"city": "北京"}))
--- study/56-谷歌实时信息搜索插件的集成与编写/google_serper_tool.py
+++ study/56-谷歌实时信息搜索插件的集成与编写/google_serper_tool.py
@@ -5,3 +5,25 @@
 @Author  : tianshiyang
 @File    : google_serper_tool.py
 """
+from typing import Any, Type
+
+import dotenv
+from langchain_community.tools import GoogleSerperRun
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from pydantic import BaseModel, Field
+
+dotenv.load_dotenv()
+
+
+class GoogleSerperArgsSchema(BaseModel):
+    query: str = Field(description="执行谷歌搜索的查询语句")
+
+
+google_serper = GoogleSerperRun(
+    name="google_serper_tool",
+    description="这是一个调用谷歌搜索引擎的tool，当你需要进行联网搜索的时候，可以调用此工具",
+    args_schema=GoogleSerperArgsSchema,
+    api_wrapper=GoogleSerperAPIWrapper()
+)
+
+print(google_serper.invoke("马拉松的世界记录是多少?"))
