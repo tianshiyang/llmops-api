a58c886a9af722c9d5e3cbfa2a3ddafc72809366
田世洋 <tianshiyang@icourt.cc>
2025-06-11 22:47:25 +0800

    feat: 删除更新示例

--- study/67-LangGraph实现ReACT架构Agent/1.预构建ReACT智能体.py
+++ study/67-LangGraph实现ReACT架构Agent/1.预构建ReACT智能体.py
@@ -5,3 +5,48 @@
 @Author  : tianshiyang
 @File    : 1.预构建ReACT智能体.py
 """
+import os
+
+import dotenv
+from langchain_community.tools import GoogleSerperRun
+from langchain_community.tools.openai_dalle_image_generation import OpenAIDALLEImageGenerationTool
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from pydantic import BaseModel, Field
+
+dotenv.load_dotenv()
+
+
+class GoogleSerperArgsSchema(BaseModel):
+    query: str = Field(description="执行谷歌搜索的查询语句")
+
+
+class DallEArgsSchema(BaseModel):
+    query: str = Field(description="输入应该是生成图像的文本提示(prompt)")
+
+
+# 1.定义工具与工具列表
+google_serper = GoogleSerperRun(
+    name="google_serper",
+    description=(
+        "一个低成本的谷歌搜索API。"
+        "当你需要回答有关时事的问题时，可以调用该工具。"
+        "该工具的输入是搜索查询语句。"
+    ),
+    args_schema=GoogleSerperArgsSchema,
+    api_wrapper=GoogleSerperAPIWrapper(),
+)
+dalle = OpenAIDALLEImageGenerationTool(
+    name="openai_dalle",
+    api_wrapper=DallEAPIWrapper(model="dall-e-3"),
+    args_schema=DallEArgsSchema,
+)
+tools = [google_serper, dalle]
+
+model = ChatOpenAI(model=os.getenv("BASE_CHAT_MODEL"))
+
+agent = create_react_agent(tools=tools, model=model)
+
+print(agent.invoke({"messages": [("human", "请帮我绘制一幅鲨鱼在天上飞的图片")]}))
--- study/68-图结构应用程序删除消息的使用技巧/1.删除与更新消息示例.py
+++ study/68-图结构应用程序删除消息的使用技巧/1.删除与更新消息示例.py
@@ -5,3 +5,45 @@
 @Author  : tianshiyang
 @File    : 1.删除与更新消息示例.py
 """
+import os
+from typing import Any
+
+import dotenv
+from langchain_core.messages import RemoveMessage, AIMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import MessagesState, StateGraph
+
+dotenv.load_dotenv()
+
+llm = ChatOpenAI(model=os.getenv("BASE_CHAT_MODEL"))
+
+
+def chatbot(state: MessagesState, config: dict) -> Any:
+    print(state["messages"])
+    return {"messages": [llm.invoke(state["messages"])]}
+
+
+def delete_human_message(state: MessagesState, config: dict) -> Any:
+    human_message = state['messages'][0]
+    return {"messages": [RemoveMessage(id=human_message.id)]}
+
+
+def update_ai_message(state: MessagesState, config: dict) -> Any:
+    message = state["messages"][-1]
+    return {"message": [AIMessage(id=message.id, content="这是修改后的内容" + message.content)]}
+
+
+graph_builder = StateGraph(MessagesState)
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_node("delete_human_message", delete_human_message)
+graph_builder.add_node("update_ai_message", update_ai_message)
+
+graph_builder.set_entry_point("chatbot")
+graph_builder.add_edge("chatbot", "delete_human_message")
+graph_builder.add_edge("delete_human_message", "update_ai_message")
+graph_builder.set_finish_point("update_ai_message")
+
+graph = graph_builder.compile()
+
+print(graph.invoke({"messages": [("human", "你好，你是")]}))
