87165bd6099ff3b4d245e4f41f0cb4a6a35bad2d
田世洋 <tianshiyang@icourt.cc>
2025-07-15 22:39:52 +0800

    feat: 全文检索

---
+++ internal/core/retrievers/__init__.py
@@ -0,0 +1,10 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 15.7.25 PM10:17
+@Author  : tianshiyang
+@File    : __init__.py.py
+"""
+from .full_text_retrievers import FullTextRetriever
+
+__all__ = ['FullTextRetriever']
