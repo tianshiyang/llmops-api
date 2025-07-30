1e7e1a057daac71debfda4b53f99045e6f1042f1
田世洋 <tianshiyang@icourt.cc>
2025-07-30 23:39:48 +0800

    feat: 总结用户输入内容

---
+++ internal/service/conversation_service.py
@@ -0,0 +1,81 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 27.7.25 PM11:53
+@Author  : tianshiyang
+@File    : conversation_service.py
+"""
+import logging
+import os
+
+from injector import inject
+from dataclasses import dataclass
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
+from internal.entity.conversation_entity import SUMMARIZER_TEMPLATE, CONVERSATION_NAME_TEMPLATE, ConversationInfo
+from internal.service.base_service import BaseService
+from pkg.sqlalchemy import SQLAlchemy
+
+
+@inject
+@dataclass
+class ConversationService(BaseService):
+    """会话服务"""
+    db: SQLAlchemy
+
+    @classmethod
+    def summary(cls, human_message: str, ai_message: str, old_summary: str = "") -> str:
+        """根据传递的人类信息，AI消息还有原始的摘要信息总结成一段新的摘要"""
+        # 1. 创建prompt
+        prompt = ChatPromptTemplate.from_template(SUMMARIZER_TEMPLATE)
+
+        # 2.构建大语言模型实例，并且将大语言模型的温度调低，降低幻觉概率
+        llm = ChatOpenAI(model=os.getenv("BASE_CHAT_MODEL"), temperature=0.5)
+
+        # 3.构建链应用
+        summary_chain = prompt | llm | StrOutputParser()
+
+        # 4. 调用链并获取新摘要的信息
+        new_summary = summary_chain.invoke({
+            "summary": old_summary,
+            "new_lines": f"Human: {human_message}\nAI: {ai_message}",
+        })
+        return new_summary
+
+    @classmethod
+    def generate_conversation_name(cls, query: str) -> str:
+        """根据传递的query生成对应的会话名字，并且语言与用户的输入保持一致"""
+        # 1. 创建prompt
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", CONVERSATION_NAME_TEMPLATE),
+            ("human", "{query}")
+        ])
+
+        # 2.构建大语言模型实例，并且将大语言模型的温度调低，降低幻觉的概率
+        llm = ChatOpenAI(model=os.getenv("BASE_CHAT_MODEL"), temperature=0)
+        structured_llm = llm.with_structured_output(ConversationInfo)
+
+        # 3.构建链应用
+        chain = prompt | structured_llm
+
+        # 4.提取并整理query，截取长度过长的部分
+        if len(query) > 2000:
+            query = query[:300] + "...[TRUNCATED]..." + query[-300:]
+        query = query.replace("\n", " ")
+
+        # 5.调用链并获取会话信息
+        conversation_info = chain.invoke({"query": query})
+
+        # 6.提取会话名称
+        name = "新的会话"
+        try:
+            if conversation_info and hasattr(conversation_info, "subject"):
+                name = conversation_info.subject
+        except Exception as e:
+            logging.exception(f"提取会话名称出错，conversation_info：{conversation_info},错误信息：{str(e)}")
+        if len(name) > 75:
+            name = name[:75] + "..."
+        return name
