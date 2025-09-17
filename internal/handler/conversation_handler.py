54fd237cb0fbb431a0f34d0f16c9f2f2413043a3
田世洋 <tianshiyang@icourt.cc>
2025-09-17 11:17:03 +0800

    feat: 获取会话消息列表分页

--- internal/handler/conversation_handler.py
+++ internal/handler/conversation_handler.py
@@ -7,11 +7,16 @@
 """
 from dataclasses import dataclass
 
-from flask_login import login_required
+from flask import request
+from flask_login import login_required, current_user
 from injector import inject
 
+from internal.schema.conversation_schema import GetConversationMessagesWithPageReq, GetConversationMessagesWithPageResp
 from internal.service import ConversationService
 from uuid import UUID
+
+from pkg.paginator.paginator import PageModel
+from pkg.response import validate_error_json, success_json
 
 
 @inject
@@ -23,4 +28,19 @@ class ConversationHandler:
     @login_required
     def get_conversation_messages_with_page(self, conversation_id: UUID):
         """根据传递的会话id获取该会话的消息列表分页数据"""
-        pass
+        # 1.提取数据并校验
+        req = GetConversationMessagesWithPageReq(request.args)
+        if not req.validate():
+            return validate_error_json(req.errors)
+
+        # 2.调用服务获取消息列表
+        messages, paginator = self.conversation_service.get_conversation_messages_with_page(
+            conversation_id,
+            req,
+            current_user
+        )
+
+        # 3.构建响应结构并返回
+        resp = GetConversationMessagesWithPageResp(many=True)
+
+        return success_json(PageModel(list=resp.dump(messages), paginator=paginator))
--- internal/service/conversation_service.py
+++ internal/service/conversation_service.py
@@ -7,6 +7,7 @@
 """
 import logging
 import os
+from datetime import datetime
 from typing import Any
 from uuid import UUID
 
@@ -17,12 +18,16 @@ from dataclasses import dataclass
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
+from sqlalchemy import desc
 
 from internal.core.agent.entities.queue_entity import AgentThought, QueueEvent
 from internal.entity.conversation_entity import SUMMARIZER_TEMPLATE, CONVERSATION_NAME_TEMPLATE, ConversationInfo, \
     SUGGESTED_QUESTIONS_TEMPLATE, SuggestedQuestions, InvokeFrom, MessageStatus
-from internal.model import Conversation, Message, MessageAgentThought
+from internal.exception import NotFoundException
+from internal.model import Conversation, Message, MessageAgentThought, Account
+from internal.schema.conversation_schema import GetConversationMessagesWithPageReq
 from internal.service.base_service import BaseService
+from pkg.paginator.paginator import Paginator
 from pkg.sqlalchemy import SQLAlchemy
 
 
@@ -31,6 +36,91 @@ from pkg.sqlalchemy import SQLAlchemy
 class ConversationService(BaseService):
     """会话服务"""
     db: SQLAlchemy
+
+    @classmethod
+    def summary(cls, human_message: str, ai_message: str, old_summary: str = "") -> str:
+        """根据传递的人类信息，AI消息还有原始的摘要信息总结成一段新的摘要"""
+        # 1. 创建prompt
+        prompt = ChatPromptTemplate.from_template(SUMMARIZER_TEMPLATE)
+
+        # 2.构建大语言模型实例，并且将大语言模型的温度调低，降低幻觉概率
+        llm = ChatOpenAI(model=os.getenv("BASE_CHAT_MODEL"), temperature=0.5)
+
+        # 3.构建链应用
+        summary_chain = prompt | llm | StrOutputParser()
+
+        # 4. 调用链并获取新摘要的信息
+        new_summary = summary_chain.invoke({
+            "summary": old_summary,
+            "new_lines": f"Human: {human_message}\nAI: {ai_message}",
+        })
+        return new_summary
+
+    @classmethod
+    def generate_conversation_name(cls, query: str) -> str:
+        """根据传递的query生成对应的会话名字，并且语言与用户的输入保持一致"""
+        # 1. 创建prompt
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", CONVERSATION_NAME_TEMPLATE),
+            ("human", "{query}")
+        ])
+
+        # 2.构建大语言模型实例，并且将大语言模型的温度调低，降低幻觉的概率
+        llm = ChatOpenAI(model=os.getenv("BASE_CHAT_MODEL"), temperature=0)
+        structured_llm = llm.with_structured_output(ConversationInfo)
+
+        # 3.构建链应用
+        chain = prompt | structured_llm
+
+        # 4.提取并整理query，截取长度过长的部分
+        if len(query) > 2000:
+            query = query[:300] + "...[TRUNCATED]..." + query[-300:]
+        query = query.replace("\n", " ")
+
+        # 5.调用链并获取会话信息
+        conversation_info = chain.invoke({"query": query})
+
+        # 6.提取会话名称
+        name = "新的会话"
+        try:
+            if conversation_info and hasattr(conversation_info, "subject"):
+                name = conversation_info.subject
+        except Exception as e:
+            logging.exception(f"提取会话名称出错，conversation_info：{conversation_info},错误信息：{str(e)}")
+        if len(name) > 75:
+            name = name[:75] + "..."
+        return name
+
+    @classmethod
+    def generate_suggested_questions(cls, histories: str) -> list[str]:
+        """根据传递的历史信息生成最多不超过3个建议问题"""
+        # 1. 创建prompt
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", SUGGESTED_QUESTIONS_TEMPLATE),
+            ("human", "{histories}")
+        ])
+
+        # 2.构建大语言模型实例，并且将大语言模型的温度降低，降低幻觉概率
+        llm = ChatOpenAI(model=os.getenv("BASE_CHAT_MODEL"), temperature=0)
+        structured_llm = llm.with_structured_output(SuggestedQuestions)
+
+        # 3.构建链应用
+        chain = prompt | structured_llm
+
+        # 4.调用链并获取建议问题列表
+        suggested_questions = chain.invoke({"histories": histories})
+
+        # 5.提取建议问题列表
+        questions = []
+        try:
+            if suggested_questions and hasattr(suggested_questions, "questions"):
+                questions = suggested_questions.questions
+        except Exception as e:
+            logging.exception(f"生成建议问题出错，suggested_questions：{suggested_questions}, 错误信息：{str(e)}")
+        if len(questions) > 3:
+            questions = questions[:3]
+
+        return questions
 
     def save_agent_thoughts(
             self,
@@ -139,87 +229,46 @@ class ConversationService(BaseService):
                         observation=agent_thought.observation,
                     )
 
-    @classmethod
-    def summary(cls, human_message: str, ai_message: str, old_summary: str = "") -> str:
-        """根据传递的人类信息，AI消息还有原始的摘要信息总结成一段新的摘要"""
-        # 1. 创建prompt
-        prompt = ChatPromptTemplate.from_template(SUMMARIZER_TEMPLATE)
+    def get_conversation(self, conversation_id: UUID, account: Account) -> Conversation:
+        """根据传递的会话id+account，获取指定的会话信息"""
+        # 1.根据conversation_id查询会话记录
+        conversation = self.get(Conversation, conversation_id)
+        if (
+                not conversation
+                or conversation.created_by != account.id
+                or conversation.is_deleted
+        ):
+            raise NotFoundException("该会话不存在或被删除，请核实后重试")
 
-        # 2.构建大语言模型实例，并且将大语言模型的温度调低，降低幻觉概率
-        llm = ChatOpenAI(model=os.getenv("BASE_CHAT_MODEL"), temperature=0.5)
+        # 2.校验通过返回会话
+        return conversation
 
-        # 3.构建链应用
-        summary_chain = prompt | llm | StrOutputParser()
+    def get_conversation_messages_with_page(
+            self,
+            conversation_id: UUID,
+            req: GetConversationMessagesWithPageReq,
+            account: Account) -> tuple[list[Message], Paginator]:
+        """根据传递的会话id+请求数据，获取当前账号下该会话的消息分页列表数据"""
+        # 1.获取会话并校验权限
+        conversation = self.get_conversation(conversation_id, account)
 
-        # 4. 调用链并获取新摘要的信息
-        new_summary = summary_chain.invoke({
-            "summary": old_summary,
-            "new_lines": f"Human: {human_message}\nAI: {ai_message}",
-        })
-        return new_summary
+        # 2.构建分页器并设置游标条件
+        paginator = Paginator(db=self.db, req=req)
+        filters = []
+        if req.created_at.data:
+            # 3.将时间戳转换成DateTime
+            created_at_datetime = datetime.fromtimestamp(req.created_at.data)
+            filters.append(Message.created_at <= created_at_datetime)
 
-    @classmethod
-    def generate_conversation_name(cls, query: str) -> str:
-        """根据传递的query生成对应的会话名字，并且语言与用户的输入保持一致"""
-        # 1. 创建prompt
-        prompt = ChatPromptTemplate.from_messages([
-            ("system", CONVERSATION_NAME_TEMPLATE),
-            ("human", "{query}")
-        ])
+        # 4.执行分页并查询数据
+        messages = paginator.paginate(
+            self.db.session.query(Message).filter(
+                Message.conversation_id == conversation.id,
+                Message.status.in_([MessageStatus.STOP, MessageStatus.NORMAL]),
+                Message.answer != "",
+                ~Message.is_deleted,
+                *filters,
+            ).order_by(desc("created_at"))
+        )
 
-        # 2.构建大语言模型实例，并且将大语言模型的温度调低，降低幻觉的概率
-        llm = ChatOpenAI(model=os.getenv("BASE_CHAT_MODEL"), temperature=0)
-        structured_llm = llm.with_structured_output(ConversationInfo)
-
-        # 3.构建链应用
-        chain = prompt | structured_llm
-
-        # 4.提取并整理query，截取长度过长的部分
-        if len(query) > 2000:
-            query = query[:300] + "...[TRUNCATED]..." + query[-300:]
-        query = query.replace("\n", " ")
-
-        # 5.调用链并获取会话信息
-        conversation_info = chain.invoke({"query": query})
-
-        # 6.提取会话名称
-        name = "新的会话"
-        try:
-            if conversation_info and hasattr(conversation_info, "subject"):
-                name = conversation_info.subject
-        except Exception as e:
-            logging.exception(f"提取会话名称出错，conversation_info：{conversation_info},错误信息：{str(e)}")
-        if len(name) > 75:
-            name = name[:75] + "..."
-        return name
-
-    @classmethod
-    def generate_suggested_questions(cls, histories: str) -> list[str]:
-        """根据传递的历史信息生成最多不超过3个建议问题"""
-        # 1. 创建prompt
-        prompt = ChatPromptTemplate.from_messages([
-            ("system", SUGGESTED_QUESTIONS_TEMPLATE),
-            ("human", "{histories}")
-        ])
-
-        # 2.构建大语言模型实例，并且将大语言模型的温度降低，降低幻觉概率
-        llm = ChatOpenAI(model=os.getenv("BASE_CHAT_MODEL"), temperature=0)
-        structured_llm = llm.with_structured_output(SuggestedQuestions)
-
-        # 3.构建链应用
-        chain = prompt | structured_llm
-
-        # 4.调用链并获取建议问题列表
-        suggested_questions = chain.invoke({"histories": histories})
-
-        # 5.提取建议问题列表
-        questions = []
-        try:
-            if suggested_questions and hasattr(suggested_questions, "questions"):
-                questions = suggested_questions.questions
-        except Exception as e:
-            logging.exception(f"生成建议问题出错，suggested_questions：{suggested_questions}, 错误信息：{str(e)}")
-        if len(questions) > 3:
-            questions = questions[:3]
-
-        return questions
+        return messages, paginator
